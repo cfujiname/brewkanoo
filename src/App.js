142417351946c258189f51183f8328a2990f443e
olliesmith3 <olliesmith3@rocketmail.com>
2020-11-10 19:14:40 +0000

    creating recipes with props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import RecipesButton from './components/RecipesButton';
-import React from 'react';
+import Recipes from './components/Recipes';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>BrewKanoo</h1>
-       <RecipesButton />
-    </div>
-  );
+class App extends Component {
+  state = {
+    recipes: [
+      {
+        id: 1,
+        title: "Ollie's Big Flavor Non-Descript Homebrew",
+        rating: 4
+      },
+      {
+        id: 2,
+        title: "Richard's No Nonsense Pale Ale",
+        rating: 8
+      },
+      {
+        id: 3,
+        title: "Linus's Hygge Winter Warmer",
+        rating: 6
+      },
+      {
+        id: 4,
+        title: "Cristina's Big Boy (PREMIUM)",
+        rating: 9
+      }
+    ]
+  }
+
+  render() {
+    console.log(this.state.recipes)
+    return (
+      <div className="App">
+        <h1>BrewKanoo</h1>
+          <Recipes recipes={this.state.recipes} />
+         {/* <RecipesButton /> */}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/components/Recipes.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Recipes extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Recipes</h1>
+      </div>
+    );
+  };
+};
+
+
+
+export default Recipes;
