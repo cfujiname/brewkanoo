db1d6ae4c6d6fe06d0e96f347d59acbd012d3fc9
Richard <rich_pryce@yahoo.co.uk>
2020-11-10 16:19:30 +0000

    recipies button added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders BrewKanoo title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("BrewKanoo");
   expect(linkElement).toBeInTheDocument();
 });
+
---
+++ src/components/RecipesButton.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class RecipesButton extends Component {
+  render() {
+return < button> Recipes </button>
+    
+  }
+}
+
+export default RecipesButton; 
