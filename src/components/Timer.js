eca9cdfc19bec36686288ef015efd7ab87e4a2fa
Richard <rich_pryce@yahoo.co.uk>
2020-11-12 15:17:05 +0000

    Timer does not show when time value is 0

---
+++ src/components/Timer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Timer extends Component {
+  render() {
+    return (
+      <div>
+        This is a timer
+      </div>
+    )
+  }
+}
+
+export default Timer
--- src/components/Walkthrough.js
+++ src/components/Walkthrough.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import Step from './Step'
+import Timer from './Timer'
 
 export class Walkthrough extends Component {
   constructor(props){
@@ -20,21 +21,35 @@ export class Walkthrough extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <h2>Method:</h2>
-        <p>
-          {console.log(this.state.counter)}
-          <Step key={this.state.counter} stepNumber={this.state.counter + 1} stepContent={this.props.walkthrough[this.state.counter].description}/>
-          {/* { this.props.walkthrough.map((step) => (
-            <Step key={this.props.walkthrough.indexOf(step)} stepNumber={this.props.walkthrough.indexOf(step) + 1} stepContent={step} />
-          ))} */}
-        </p>
-        <button onClick={this.handlePreviousStep}>Previous Step</button>
-        <button onClick={this.handleNextStep}>Next Step</button>
-       
-      </div>
-    )
+    if (this.props.walkthrough[this.state.counter].time !== 0) {
+      return (
+        <div>
+          <h2>Method:</h2>
+          <p>
+            {console.log(this.state.counter)}
+            <Step key={this.state.counter} stepNumber={this.state.counter + 1} stepContent={this.props.walkthrough[this.state.counter].description} />
+          
+          </p>
+          <button onClick={this.handlePreviousStep}>Previous Step</button>
+          <button onClick={this.handleNextStep}>Next Step</button>
+          <Timer />
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h2>Method:</h2>
+          <p>
+            {console.log(this.state.counter)}
+            <Step key={this.state.counter} stepNumber={this.state.counter + 1} stepContent={this.props.walkthrough[this.state.counter].description} />
+          
+          </p>
+          <button onClick={this.handlePreviousStep}>Previous Step</button>
+          <button onClick={this.handleNextStep}>Next Step</button>
+        </div>
+      )
+    }
+
   }
 }
 
