da735af2e22194948d9d7f92850f81c569f94014
Linus Mjornstedt <carl.mjornstedt@gmail.com>
2020-11-12 15:25:15 +0000

    Added timer code, needs refactoring

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,13 +1,45 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react';
 
-export class Timer extends Component {
-  render() {
-    return (
-      <div>
-        This is a timer
-      </div>
-    )
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  function toggle() {
+    setIsActive(!isActive);
   }
-}
 
-export default Timer
+  function reset() {
+    setSeconds(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  return (
+    <div className="app">
+      <div className="time">
+        {seconds}s
+      </div>
+      <div className="row">
+        <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
+          {isActive ? 'Pause' : 'Start'}
+        </button>
+        <button className="button" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Timer;
