ff25ff0d3aaa9c0e4c697fd0128c6ae7fff4548c
Linus Mjornstedt <carl.mjornstedt@gmail.com>
2020-11-13 14:59:02 +0000

    wrote test for Hide button, working

--- src/tests/RecipeItem.test.js
+++ src/tests/RecipeItem.test.js
@@ -6,7 +6,7 @@ import RecipeItem from '../components/RecipeItem.js';
 it("should show a clickable hide button after a recipe is clicked", () => {
     const mockRecipe = {
         id: 1,
-        title: "Ollie's Big Flavor Non-Descript Homebrew",
+        title: "Mock Recipe 1",
         walkthrough: [
           { description: "Boil the water", time: 10},
         ],
@@ -21,9 +21,31 @@ it("should show a clickable hide button after a recipe is clicked", () => {
 
     const button = screen.getByRole('button');
     button.click();
-    screen.debug();
     const buttons = screen.getAllByRole('button');
     expect(buttons).toHaveLength(4);
+});
 
+it("should show the beer buttons after the hide button is clicked.",() => {
+    const mockRecipe2 = {
+        id: 2,
+        title: "Mock Recipe 2",
+        walkthrough: [
+          { description: "Boil the water", time: 10},
+        ],
+        ingredients: {
+          grains: ["chocolate malt", "crystal malt"],
+          hops: ["Bramling Cross", "Citra"],
+          yeast: "Safeale",
+        },
+        equipment: ["A bucket","Thermometer"]
+      }
+      render(<RecipeItem recipeProp={mockRecipe2}/>)
+      const button = screen.getByRole('button');
+      button.click();
+      const hideButton = screen.getByText("Hide")
+      hideButton.click();
+      const button1 = screen.getAllByRole('button');
+      expect(button1).toHaveLength(1);
+      
 
 });
