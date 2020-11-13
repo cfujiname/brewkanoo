9b2f4877c736ab11d1c7066fcbc069c41422a4b0
Richard <rich_pryce@yahoo.co.uk>
2020-11-13 11:21:15 +0000

    Walkthrough tests for next button

--- src/tests/Walkthrough.test.js
+++ src/tests/Walkthrough.test.js
@@ -13,3 +13,45 @@ it("should display the next step when the Next Step button is clicked", () => {
   expect(secondStep).toBeInTheDocument();
 });
 
+it("should display the previous step when the Previous Step button is clicked", () => {
+  render(<Walkthrough walkthrough={[
+    { description: "Boil the water", time: 10},
+    { description: "Add the teabag", time: 0}
+  ]}/>);
+  const nextStepButton = screen.getByText('Next Step')
+  const prevStepButton = screen.getByText('Previous Step')
+  nextStepButton.click();
+  prevStepButton.click();
+
+  const firstStep = screen.getByText('1: Boil the water');
+  expect(firstStep).toBeInTheDocument();
+});
+
+it("should display the last step when the Next Step button is clicked multiple times", () => {
+  render(<Walkthrough walkthrough={[
+    { description: "Boil the water", time: 10},
+    { description: "Add the teabag", time: 0}
+  ]}/>);
+  const nextStepButton = screen.getByText('Next Step')
+    var array = [1,2,3]
+  array.forEach(function () { nextStepButton.click() })
+  
+  const secondStep = screen.getByText('2: Add the teabag');
+  expect(secondStep).toBeInTheDocument();
+});
+
+it("should display the timer when the time data is > 0", () => {
+  render(<Walkthrough walkthrough={[
+    { description: "Boil the water", time: 0},
+    { description: "Add the teabag", time: 10}
+  ]}/>);
+  const nextStepButton = screen.getByText('Next Step')
+  
+  nextStepButton.click();
+  const startButton = screen.getByText('Start')
+  expect(startButton).toBeInTheDocument()
+  
+});
+
+
+
