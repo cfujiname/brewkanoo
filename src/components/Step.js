e1f95315ec2c8c07ed9dc996e89b2c4a9c3807d5
olliesmith3 <olliesmith3@rocketmail.com>
2020-11-13 16:46:07 +0000

    Added calculator for strikeWater on step component

--- src/components/Step.js
+++ src/components/Step.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
 export class Step extends Component {
+  hasStrikeWater() {
+    if (this.props.stepContent.strikeWater) {
+      return ": " + (this.props.strikeWater * 0.8).toFixed(2) + " Litres"
+    }
+  }
+
   render() {
     return (
       <div>
-        {this.props.stepNumber}: {this.props.stepContent.description}: {this.props.strikeWater}
+        {this.props.stepNumber}: {this.props.stepContent.description} {this.hasStrikeWater()}
       </div>
     )
   }
--- src/components/Walkthrough.js
+++ src/components/Walkthrough.js
@@ -6,15 +6,13 @@ import TestTimer from './TestTimer'
 export class Walkthrough extends Component {
   constructor(props){
     super(props);
-    this.state = { counter: 0, start: false} //stepIndex = 0
+    this.state = { counter: 0, start: false} 
   }
-
 
   handleNextStep = () => {
       if (this.state.counter < this.props.walkthrough.length - 1) {
     this.setState(prev => ({ counter: prev.counter + 1 }));
       } 
-    //this.stepIndex = this.stepIndex + 1
   }
   handlePreviousStep = () => {
       if (this.state.counter > 0)
@@ -24,34 +22,24 @@ export class Walkthrough extends Component {
     this.setState({ start: true })
   }
 
-  render() {
+  timer() {
     if (this.props.walkthrough[this.state.counter].time !== 0) {
-      return (
-        <div>
-          <h1>{this.props.potVolume}</h1>
-          <h2>Method:</h2>
-            <Step key={this.state.counter} stepNumber={this.state.counter + 1} stepContent={this.props.walkthrough[this.state.counter]} strikeWater={this.props.potVolume} />
-          
-          <button onClick={this.handlePreviousStep}>Previous Step</button>
-          <button onClick={this.handleNextStep} >Next Step</button>
-
-        
-         <TestTimer timerStart={this.props.walkthrough[this.state.counter].time}/>    
-      
-          
-        </div>
-      )
+      return <TestTimer timerStart={this.props.walkthrough[this.state.counter].time}/> 
     } else {
-      return (
-        <div>
-          <h2>Method:</h2>
-          <Step key={this.state.counter} stepNumber={this.state.counter + 1} stepContent={this.props.walkthrough[this.state.counter]} strikeWater={this.props.potVolume} />
-          <button onClick={this.handlePreviousStep}>Previous Step</button>
-          <button onClick={this.handleNextStep}>Next Step</button>
-        </div>
-      )
+      return null
     }
+  }
 
+  render() {
+    return (
+      <div>
+        <h2>Method:</h2>
+          <Step key={this.state.counter} stepNumber={this.state.counter + 1} stepContent={this.props.walkthrough[this.state.counter]} strikeWater={this.props.potVolume} />  
+        <button onClick={this.handlePreviousStep}>Previous Step</button>
+        <button onClick={this.handleNextStep} >Next Step</button>
+        {this.timer()}   
+      </div>
+    )
   }
 }
 
