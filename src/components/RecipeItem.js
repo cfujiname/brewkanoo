1e6c9d78eaa4ae9ad61f5c0e38a178b15fda211e
Richard <rich_pryce@yahoo.co.uk>
2020-11-13 15:23:48 +0000

    Volume form takes an input and then goes to walkthrough

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import Walkthrough from './Walkthrough';
-import Ingredients from './Ingredients';
-import Equipment from './Equipment';
+
 import VolumeForm from './VolumeForm';
 
 export class RecipeItem extends Component {
@@ -26,16 +24,14 @@ export class RecipeItem extends Component {
   }
 
   //insert item here for clicking off the title - to remove the Details 
-
+  
   render() {
     if (this.state.showDetails) {
       return (
         <div>
-          <VolumeForm potVolume={this.state.potVolume} />
           <button onClick={this.handleClickOffTitle}>Hide</button>
-          <Walkthrough key={this.props.recipeProp.id} walkthrough={this.props.recipeProp.walkthrough} />
-          <Ingredients key={this.props.recipeProp.id} ingredients={this.props.recipeProp.ingredients}/>
-          <Equipment key={this.props.recipeProp.id} equipment={this.props.recipeProp.equipment}/>
+          <VolumeForm recipe={this.props.recipeProp}/>
+         
         </div>
       );
     } else {
--- src/components/VolumeForm.js
+++ src/components/VolumeForm.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react'
+import Walkthrough from './Walkthrough';
+import Ingredients from './Ingredients';
+import Equipment from './Equipment';
 
 export class VolumeForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {potVolume: 25};
+    this.state = {potVolume: 25, showDetails: false};
 
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  handleClickTitle = () => {
+    this.setState({
+      showDetails: true
+    })
+  }
+
+  handleClickOffTitle = () => {
+    this.setState({
+      
+      showDetails: false
+      
+    })
+  }
+
+
   onSubmit(event) {
     event.preventDefault();
     this.setState({
-      potVolume: 30
+     showDetails: true
     })
     console.log(this.state.potVolume)
   }
@@ -19,13 +37,18 @@ export class VolumeForm extends Component {
   render() {
     return (
       <div>
-        <form onClick={this.onSubmit}>
+        <form >
           <label>
             Boiling water pot volume:
             <input type="number" name="pot-volume" />
           </label>
-          <input type="submit" value="Submit" />
+          <input onClick={this.onSubmit} type="submit" value="Submit" />
         </form>
+        {this.state.showDetails ? <Walkthrough key={this.props.recipe.id} walkthrough={this.props.recipe.walkthrough} /> : null} 
+          
+           {/* <Ingredients key={this.props.recipeProp.id} ingredients={this.props.recipeProp.ingredients}/>
+           <Equipment key={this.props.recipeProp.id} equipment={this.props.recipeProp.equipment}/> : null} */}
+     
       </div>
     )
   }
