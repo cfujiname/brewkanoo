44c8105384e1ef9e6a08663fe50570eb044594f9
Richard <rich_pryce@yahoo.co.uk>
2020-11-18 20:44:24 +0000

    Changed css from fixed to absolute and compleated alert now working

--- src/components/CompletedButton.js
+++ src/components/CompletedButton.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Alert from 'react-bootstrap/Alert'
 
 export class CompletedButton extends Component {
   constructor(props) {
@@ -19,11 +20,22 @@ export class CompletedButton extends Component {
 
 
   render() {
-    return (
-      <div class="completed-button">
-        <button onClick={this.completed} >{this.state.complete ? "Certainly!" : "Working on it...."}</button>
-      </div>
-    )
+    if (this.props.stepNumber !== 9) {
+      return (
+        <div class="completed-button">
+        
+          <button onClick={this.completed} >{this.state.complete ? "Certainly" : "Working on it...."}</button>
+        </div>
+      )
+    } else {
+      return (
+        <div class="completed-button">
+        
+          <button onClick={this.completed} >{this.state.complete ? <Alert.Heading>Well done - Enjoy your beer!</Alert.Heading> : "Working on it...."}</button>
+        </div>
+      )
+      
+    }
   }
 }
 
