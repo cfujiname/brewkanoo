6de816dcf4abf3d1be2b118a9b124e7b294d6374
cristina <c.fujiname@gmail.com>
2020-11-20 11:21:37 +0000

    Equipment test refactored

--- src/tests/Equipment.test.js
+++ src/tests/Equipment.test.js
@@ -2,10 +2,8 @@ import { render, screen } from '@testing-library/react';
 import Equipment from '../components/Equipment'
 
 it("should display equipments when Equipment button is clicked", () => {
-  render(<Equipment equipment={["A large stock pot","Thermometer", "Fermentation Vessel","Airlock","Hydrometer","Bottles(with caps and capper)/Keg","Siphon tube","Mashing/Sparging bag"]
-  }/>);
-  const equipmentButton = screen.getByRole('heading')
-  equipmentButton.click();
+  const mockEquipment = ["A large stock pot","Thermometer", "Fermentation Vessel","Airlock","Hydrometer","Bottles(with caps and capper)/Keg","Siphon tube","Mashing/Sparging bag"];
+  render(<Equipment equipment={mockEquipment}/>);
   const equipmentList = screen.getByText('Thermometer');
   expect(equipmentList).toBeInTheDocument();
 });
--- src/tests/Step.test.js
+++ src/tests/Step.test.js
@@ -1,10 +1,24 @@
-it("should display the next step when the Next Step button is clicked", () => {
-  render(<Walkthrough walkthrough={[
-    { description: "Boil the water", time: 10},
-    { description: "Add the teabag", time: 0}
-  ]}/>);
-  const nextStepButton = screen.getByText('Next Step')
-  nextStepButton.click();
-  const secondStep = screen.getByText('2: Add the teabag');
-  expect(secondStep).toBeInTheDocument();
-});
+import React from 'react';
+import Step from '../components/Step';
+import { render, screen, getByLabelText } from '@testing-library/react';
+
+// it("should display the Steps when Walkthrough button is clicked", () => {
+//   const mockKey = "1";
+//   const mockStepNumber = "1";
+//   const mockStepContent = "test content";
+//   const mockBatchSize = "19";
+//   const mockSpecs = { mashTemp: 68, colour: '#FFF897'};
+//   const mockIngredients = {
+//     grains: [{ item: "Great Western Wheat Malt", quantity: 2.2 }, { item: "Durst Pilsner Malt", quantity: 2.2 }],
+//     hops: [ { item: "Hallertau Hops", quantity: 17, type: "Bittering" },
+//             { item: "Cascade Hops", quantity: 50, type: "Flavouring" },
+//             { item: "Citra Hops", quantity: 57, type: "Aroma" }
+//           ],
+//     yeast: {item: "Safale US-05 yeast", quantity: 1 },
+//     primingSugar: 3.3
+//   }; 
+
+//   render(<Step key={mockKey} stepNumber={mockStepNumber} stepContent={mockStepContent} batchSize={mockBatchSize} specs={mockSpecs} ingredients={mockIngredients} />);
+//   });
+
+
